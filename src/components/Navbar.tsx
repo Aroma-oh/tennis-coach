523a9dce796b3b8e70c7161271f84608dccb80bd
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-11 22:58:14 +0900

    feat: NavBar 컴포넌트 구현

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+import { Button } from './Button';
+interface NavbarProps {
+  brandName: string;
+  buttonText: string;
+}
+export const Navbar = ({ brandName, buttonText }: NavbarProps) => {
+  return (
+    <NavbarStyled>
+      <h1 className='title'>{brandName}</h1>
+      <div className='button'>
+        <Button buttonText={buttonText} color={false} />
+
+      </div>
+    </NavbarStyled>)
+}
+
+const NavbarStyled = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  height: 94px;
+  padding: 0px 64px;
+
+  border-bottom: 1px solid var(--black);
+
+  @media (max-width: 400px) {
+    justify-content: center;
+    .button {
+      display: none;
+    }
+  }
+`
