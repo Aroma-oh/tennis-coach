9aacb0a5ef03daff73a51aae0427e46fd88da652
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-13 22:56:25 +0900

    feat: 미디어 쿼리 추가

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -30,7 +30,7 @@ export const Contact = () => {
             onChange={handlePhone}
           />
         </div>
-        <Button buttonText='등록하기' color={true} padding='18px 48px' />
+        <Button buttonText='등록하기' color={true} padding='21px 48px' />
       </form>
     </ContactStyled>
   )
@@ -40,18 +40,21 @@ const ContactStyled = styled.section`
   display: flex;
   flex-direction: column;
   align-items: center;
-
+  padding: 56px 0 24px 0;
   h1 {
     font-size: 48px;
     font-weight: 600;
     text-align: center;
+    line-height: 130%;
   }
   .sub-head {
     font-size: 24px;
     font-weight: 600;
     color: var(--gray);
-    padding: 36px 24px 58px 24px;
+    padding: 24px 24px 58px 24px;
     text-align: center;
+    line-height: 150%;
+    word-break: keep-all;
   }
   form {
     display: flex;
@@ -76,5 +79,31 @@ const ContactStyled = styled.section`
   }
   button {
     margin: 32px 0 5rem;
+  }
+
+  @media(max-width: 500px) {
+    padding: 5rem 1rem;
+    h1 {
+      font-size: 32px;
+    }
+    .sub-head {
+      font-size: 18px;
+      padding: 28px 0 48px 0;
+    }
+    .input-container {
+      flex-direction: column;
+      padding: 8px;
+    }
+    .input-type{
+      width: 100%;
+    }
+    input {
+      width: 230px;
+      margin-top: 12px;
+    }
+    button {
+      margin-top: 18px;
+      width: 246px;
+    }
   }
 `
