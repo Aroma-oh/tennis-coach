e4284a80f5f8123eb28c9d9199e6a75427dcde8e
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-13 22:04:00 +0900

    feat: Feature4 컴포넌트 생성

---
+++ src/sections/features/Feature4.tsx
@@ -0,0 +1,53 @@
+import styled from 'styled-components'
+
+export const Feature4 = () => {
+  return (
+    <Feature4Styled>
+      <div className='title'><img src='public/sparkle.png' width={24} />&nbsp;시설 미리보기</div>
+      <div className='image-container'>
+        <div
+          className='image'
+          style={{ backgroundImage: `url('image2.png')` }} />
+        <div
+          className='image'
+          style={{ backgroundImage: `url('image1.png')` }} />
+      </div>
+
+    </Feature4Styled>
+  )
+}
+
+const Feature4Styled = styled.section`
+  width: 100%;
+  margin: 8rem 0;
+
+  .title {
+    font-size: 21px;
+    font-weight: 500;
+    padding: 12px 18px;
+    background-color: var(--white);
+    border: 1px solid black;
+    width: fit-content;
+
+    display: flex;
+    align-items: center;
+  }
+  .image-container {
+    margin-top: 24px;
+    height: 400px;
+    display: flex;
+  }
+  .image {
+    display: block;
+    width: 50vw;
+    height: 40vh;
+    margin-right: 12px;
+    border: 1px solid black;
+    background-size: cover;
+    background-position: center;
+  }
+
+  @media (max-width: 500px) {
+    display: none;
+  }
+`
