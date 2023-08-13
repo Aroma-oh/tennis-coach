42be51ff7f1c99d72bc91c018cf5597f1b8126bf
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-13 22:04:22 +0900

    feat: Button 컴포넌트 수정

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -4,16 +4,18 @@ interface ButtonProps {
   buttonText: string;
   color: boolean;
   padding?: string;
+  fonsSize?: number;
 }
-export const Button = ({ buttonText, color, padding }: ButtonProps) => {
+export const Button = ({ buttonText, color, padding, fonsSize }: ButtonProps) => {
   return (
-    <ButtonStyled color={color} padding={padding}>{buttonText}</ButtonStyled>
+    <ButtonStyled color={color} padding={padding} fonsSize={fonsSize}> {buttonText} </ButtonStyled>
   )
 }
 
 interface ButtonStyledProps {
   color: boolean;
   padding?: string;
+  fonsSize?: number;
 }
 
 const ButtonStyled = styled.button<ButtonStyledProps>`
@@ -22,9 +24,10 @@ const ButtonStyled = styled.button<ButtonStyledProps>`
   height: 24px;
     
   color: ${({ color }) => color ? 'var(--white, #FFF)' : 'var(--black, #000)'};
-  font-size: 16px;
+  font-size: ${({ fonsSize }) => fonsSize ? `${fonsSize}px` : '16px'};
   line-height: 0px; 
 
   border: 1px solid var(--black, #000);
   background-color: ${({ color }) => color ? 'var(--black, #000)' : 'var(--white, #FFF)'};
+  cursor: pointer;
 `
