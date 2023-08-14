03a0783a9148e71ce044c6cda9d329a6d82a0083
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-14 14:02:39 +0900

    feat: vite-tsconfig-paths ssr 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
-import { ssr } from 'vite-plugin-ssr/plugin';
 
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    ssr({ prerender: true })
   ],
 });
