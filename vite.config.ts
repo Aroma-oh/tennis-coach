4e01d00da7005c2e5aaa6ec0a3b3127188148b0c
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-14 12:20:56 +0900

    setting: vite-plugin-ssr

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import react from '@vitejs/plugin-react';
+import { ssr } from 'vite-plugin-ssr/plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    ssr({
+      prerender: true,
+    }),
+  ],
+});
